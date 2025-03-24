7d919917cdd5ece3699df42ddc3b027ad0772446
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2025-03-24 18:22:16 +0100

    Company overview now displaying No Data instead of Not Reported

--- src/components/companies/detail/CompanyOverview.tsx
+++ src/components/companies/detail/CompanyOverview.tsx
@@ -156,10 +156,9 @@ export function CompanyOverview({
             <Text className="text-3xl lg:text-6xl md:text-4xl sm:text-3xl font-light text-orange-2 tracking-tighter leading-none">
             {
               selectedPeriod.emissions?.calculatedTotalEmissions === 0
-              ? t("companies.overview.notReported")
+              ? t("companies.overview.noData")
               : localizeUnit(selectedPeriod.emissions?.calculatedTotalEmissions, currentLanguage)
             }
-
               <span className="text-lg lg:text-2xl md:text-lg sm:text-sm ml-2 text-grey">
                 {t(selectedPeriod.emissions?.calculatedTotalEmissions === 0 ? ' ' : "companies.overview.tonsCO2e")}
               </span>
