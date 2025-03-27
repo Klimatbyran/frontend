8dfdde6c4597a819aaeaf075cdc2effd9d726819
Moritz Zingg <moritz.zingg@netlight.com>
2025-03-27 15:23:38 +0100

    Fixed input values shown as placeholders and autoselect newest period in the editor

--- src/pages/CompanyEditPage.tsx
+++ src/pages/CompanyEditPage.tsx
@@ -35,6 +35,11 @@ export function CompanyEditPage() {
       return periods;
   }, [] as CompanyDetails["reportingPeriods"]) : [];
 
+  if(selectedPeriods.length === 0 && company?.reportingPeriods.length !== undefined && company.reportingPeriods.length > 0) {
+    company.reportingPeriods.sort((a, b) => b.endDate > a.endDate ? -1 : 1);
+    selectedPeriods.push(company.reportingPeriods[company.reportingPeriods.length - 1]);
+  }
+
   if (loading || isUpdating) {
     return (
       <div className="animate-pulse space-y-16">
