65ecf3e2c4e25e68157e8b39899191d799755544
CarlWiren90 <156791432+CarlWiren90@users.noreply.github.com>
2025-04-03 14:29:42 +0200

    Added translation to total and changed conditional from entry.name to entry.dataKey

--- src/components/companies/detail/CustomTooltip.tsx
+++ src/components/companies/detail/CustomTooltip.tsx
@@ -17,15 +17,14 @@ export const CustomTooltip = ({
   const { t } = useTranslation();
   const { getCategoryName } = useCategoryMetadata();
   const { currentLanguage} = useLanguage();
-
-  if (active && payload && payload.length) {
     
+  if (active && payload && payload.length) {
     if (payload.length === 3) {
       const totalEmissions = payload[0]?.payload.total;
       
       const companyTotal = {
         dataKey: 'total',
-        name: 'Total',
+        name: t('companies.emissionsHistory.total'),
         color: "white",
         payload: {
           total: totalEmissions,
@@ -59,7 +58,15 @@ export const CustomTooltip = ({
                 )}`;
 
           return (
-            <div key={entry.dataKey} className={`${entry.name === 'Total' ? 'my-2' : 'my-0'} text-grey mr-2 text-sm`}>
+            <div key={entry.dataKey} className={`
+              ${
+                entry.dataKey ==="total"
+                  ? "my-2 font-medium"
+                  : "my-0"
+              } 
+              text-grey mr-2 text-sm
+            `}
+          >
               <span className="text-grey mr-2">{name}:</span>
               <span style={{ color: entry.color }}>{displayValue}</span>
             </div>
