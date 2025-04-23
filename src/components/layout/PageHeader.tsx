77900c52b6e0c24802fd5731d54aad24a29b091b
elvbom <ecdb@protonmail.com>
2025-04-23 13:57:51 +0200

    decrease margins and spaces

--- src/components/layout/PageHeader.tsx
+++ src/components/layout/PageHeader.tsx
@@ -17,12 +17,7 @@ export function PageHeader({
   return (
     <div>
       <div className="absolute inset-0 w-screen bg-black left-0 right-0 -z-10 h-full" />
-      <div
-        className={cn(
-          "max-w-[1200px] mx-auto px-4 pt-6 pb-2 space-y-2",
-          className,
-        )}
-      >
+      <div className={cn("max-w-[1200px] mx-auto p-4 space-y-2", className)}>
         <h1 className="text-3xl font-light">{title}</h1>
         {description && <p className="text-sm text-grey">{description}</p>}
         {children && <div className="flex flex-wrap gap-3">{children}</div>}
