6c307fee96b463fd196421173b737a4fe17c44da
Carl <carlwiren90@gmail.com>
2025-08-28 08:29:45 +0200

    conditional check for totalEmissions not being null, undefined, 0 or NaN

--- src/components/ui/relatableNumbers.tsx
+++ src/components/ui/relatableNumbers.tsx
@@ -12,24 +12,27 @@ const RelatableNumbers = ({
   currentLanguage,
 }: RelatableNumbersProps) => {
   const { t } = useTranslation();
+  const validTotalEmissions = totalEmissions || null;
+
   return (
-    <div className="bg-black-2 rounded-level-1 p-16">
-      <Text variant={"h3"}>From Tons to T-Shirts: Relatable Numbers</Text>
-      <Text
-        variant="body"
-        className="text-sm md:text-base lg:text-lg max-w-3xl mt-2"
-      >
-        The current total emission of{" "}
-        <span className="text-orange-2">
-          
-          {formatEmissionsAbsolute(totalEmissions, currentLanguage)}
-        </span>
-        <span className="text-sm md:text-base lg:text-lg ml-2 text-grey">
-          {t(totalEmissions ? "emissionsUnit" : " ")}
-        </span>{" "}
-        compares to the following:
-      </Text>
-    </div>
+    validTotalEmissions && (
+      <div className="bg-black-2 rounded-level-1 p-16">
+        <Text variant={"h3"}>From Tons to T-Shirts: Relatable Numbers</Text>
+        <Text
+          variant="body"
+          className="text-sm md:text-base lg:text-lg max-w-3xl mt-2"
+        >
+          The current total emission of{" "}
+          <span className="text-orange-2">
+            {formatEmissionsAbsolute(validTotalEmissions, currentLanguage)}
+          </span>
+          <span className="text-sm md:text-base lg:text-lg ml-2 text-grey">
+            {t(validTotalEmissions ? "emissionsUnit" : " ")}
+          </span>{" "}
+          compares to the following:
+        </Text>
+      </div>
+    )
   );
 };
 
