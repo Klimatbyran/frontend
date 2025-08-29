dc4e7e31d879827432392b06d9bd73334106e945
elvbom <ecdb@protonmail.com>
2025-08-29 12:03:28 +0200

    hide trend explanation from company emissions graph for now

--- src/components/companies/detail/history/EmissionsHistory.tsx
+++ src/components/companies/detail/history/EmissionsHistory.tsx
@@ -181,7 +181,8 @@ export function EmissionsHistory({
           />
 
           {/* Method Description - Hidden on mobile (mobile has popup version) */}
-          {methodExplanation && !isMobile && (
+          {/* FIXME: Uncomment when the Trend Analysis is ready */}
+          {/* {methodExplanation && !isMobile && (
             <div className="bg-black-2 rounded-lg p-4 max-w-4xl mx-auto">
               <Text
                 variant="body"
@@ -193,7 +194,7 @@ export function EmissionsHistory({
                 {methodExplanation}
               </Text>
             </div>
-          )}
+          )} */}
         </SectionWithHelp>
       )}
       {exploreMode && (
