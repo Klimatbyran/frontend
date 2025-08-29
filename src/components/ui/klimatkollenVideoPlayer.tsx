744057c4c5f8fb06e6e60c3957756fd18c04902b
Carl <carlwiren90@gmail.com>
2025-08-29 09:07:45 +0200

    Added bg to container

--- src/components/ui/klimatkollenVideoPlayer.tsx
+++ src/components/ui/klimatkollenVideoPlayer.tsx
@@ -2,7 +2,7 @@ const KlimatkollenVideo = () => {
   return (
     <div className="w-full h-72 py-4 md:py-6 sm:h-96 lg:h-screen">
       <iframe
-        className="rounded-2xl"
+        className="rounded-2xl bg-[#121212]"
         width="100%"
         height="100%"
         src="https://www.youtube.com/embed/oRm2W7LiwME?si=PN9bT_8JTvLkS9Kg?controls=0"
