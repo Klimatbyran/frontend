1f9fe818b4163a1a449301cf2b8a2d33c18afccd
Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>
2025-09-25 11:27:56 +0200

    update eslintconfig and needed packages

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 import importPlugin from "eslint-plugin-import";
 
@@ -15,6 +16,7 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react: react,
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
       import: importPlugin,
