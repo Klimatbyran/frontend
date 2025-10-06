41ffee66f99da8fa9613c0b61f4449f706dda93e
Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>
2025-10-06 10:54:29 +0200

    update order of kpis on ranked municipalities page (#1094)

--- src/hooks/municipalities/useMunicipalityKPIs.ts
+++ src/hooks/municipalities/useMunicipalityKPIs.ts
@@ -41,6 +41,39 @@ export const useMunicipalityKPIs = (): KPIValue[] => {
       higherIsBetter: false,
     },
     {
+      label: t("municipalities.list.kpis.totalConsumptionEmission.label"),
+      key: "totalConsumptionEmission",
+      unit: "t",
+      source: "municipalities.list.totalConsumptionEmission.source",
+      sourceUrls: ["https://konsumtionskompassen.se/"],
+      description: t(
+        "municipalities.list.kpis.totalConsumptionEmission.description",
+      ),
+      detailedDescription: t(
+        "municipalities.list.kpis.totalConsumptionEmission.detailedDescription",
+      ),
+      higherIsBetter: false,
+    },
+    {
+      label: t("municipalities.list.kpis.climatePlan.label"),
+      key: "climatePlan",
+      unit: "",
+      source: "municipalities.list.climatePlan.source",
+      sourceUrls: ["https://docs.google.com/spreadsheets/d/13CMqmfdd6QUD6agKFyVhwZUol4PKzvy253_EwtsFyvw/edit#gid=0"],
+      description: t("municipalities.list.kpis.climatePlan.description"),
+      detailedDescription: t(
+        "municipalities.list.kpis.climatePlan.detailedDescription",
+      ),
+      higherIsBetter: true,
+      isBoolean: true,
+      booleanLabels: {
+        true: t("municipalities.list.kpis.climatePlan.booleanLabels.true"),
+        false: t("municipalities.list.kpis.climatePlan.booleanLabels.false"),
+      },
+      belowString: t("municipalities.list.kpis.climatePlan.belowString"),
+      aboveString: t("municipalities.list.kpis.climatePlan.aboveString"),
+    },
+    {
       label: t("municipalities.list.kpis.electricCarChangePercent.label"),
       key: "electricCarChangePercent",
       unit: "%",
@@ -56,20 +89,6 @@ export const useMunicipalityKPIs = (): KPIValue[] => {
         "municipalities.list.kpis.electricCarChangePercent.nullValues",
       ),
       higherIsBetter: true,
-    },
-    {
-      label: t("municipalities.list.kpis.totalConsumptionEmission.label"),
-      key: "totalConsumptionEmission",
-      unit: "t",
-      source: "municipalities.list.totalConsumptionEmission.source",
-      sourceUrls: ["https://konsumtionskompassen.se/"],
-      description: t(
-        "municipalities.list.kpis.totalConsumptionEmission.description",
-      ),
-      detailedDescription: t(
-        "municipalities.list.kpis.totalConsumptionEmission.detailedDescription",
-      ),
-      higherIsBetter: false,
     },
     {
       label: t("municipalities.list.kpis.electricVehiclePerChargePoints.label"),
@@ -104,26 +123,7 @@ export const useMunicipalityKPIs = (): KPIValue[] => {
         "municipalities.list.kpis.bicycleMetrePerCapita.detailedDescription",
       ),
       higherIsBetter: true,
-    },
-    {
-      label: t("municipalities.list.kpis.climatePlan.label"),
-      key: "climatePlan",
-      unit: "",
-      source: "municipalities.list.climatePlan.source",
-      sourceUrls: ["https://docs.google.com/spreadsheets/d/13CMqmfdd6QUD6agKFyVhwZUol4PKzvy253_EwtsFyvw/edit#gid=0"],
-      description: t("municipalities.list.kpis.climatePlan.description"),
-      detailedDescription: t(
-        "municipalities.list.kpis.climatePlan.detailedDescription",
-      ),
-      higherIsBetter: true,
-      isBoolean: true,
-      booleanLabels: {
-        true: t("municipalities.list.kpis.climatePlan.booleanLabels.true"),
-        false: t("municipalities.list.kpis.climatePlan.booleanLabels.false"),
-      },
-      belowString: t("municipalities.list.kpis.climatePlan.belowString"),
-      aboveString: t("municipalities.list.kpis.climatePlan.aboveString"),
-    },
+    }
   ];
 
   return KPIs;
