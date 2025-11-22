74823bc9ca11a98e5e426acdc92aea4dfe5a7edd
elvbom <ecdb@protonmail.com>
2025-11-22 11:20:28 +0100

    catch muni is null in header stats hook

--- src/hooks/municipalities/useMunicipalityDetails.tsx
+++ src/hooks/municipalities/useMunicipalityDetails.tsx
@@ -16,39 +16,41 @@ export function useMunicipalityDetailHeaderStats(
   const { t } = useTranslation();
   const { currentLanguage } = useLanguage();
 
-  const stats: DetailStat[] = [
-    {
-      label: t("municipalityDetailPage.totalEmissions", {
-        year: lastYear,
-      }),
-      value: lastYearEmissionsTon,
-      unit: t("emissionsUnit"),
-      valueClassName: "text-orange-2",
-      info: true,
-      infoText: t("municipalityDetailPage.totalEmissionsTooltip"),
-    },
-    {
-      label: t("municipalityDetailPage.annualChangeSince2015"),
-      value: formatPercentChange(
-        municipality.historicalEmissionChangePercent,
-        currentLanguage,
-      ),
-      valueClassName: cn(
-        municipality.historicalEmissionChangePercent > 0
-          ? "text-pink-3"
-          : "text-orange-2",
-      ),
-    },
-    {
-      label: t("municipalityDetailPage.consumptionEmissionsPerCapita"),
-      value: localizeUnit(
-        municipality.totalConsumptionEmission,
-        currentLanguage,
-      ),
-      valueClassName: "text-orange-2",
-      unit: t("emissionsUnit"),
-    },
-  ];
+  const stats: DetailStat[] = municipality
+    ? [
+        {
+          label: t("municipalityDetailPage.totalEmissions", {
+            year: lastYear,
+          }),
+          value: lastYearEmissionsTon,
+          unit: t("emissionsUnit"),
+          valueClassName: "text-orange-2",
+          info: true,
+          infoText: t("municipalityDetailPage.totalEmissionsTooltip"),
+        },
+        {
+          label: t("municipalityDetailPage.annualChangeSince2015"),
+          value: formatPercentChange(
+            municipality.historicalEmissionChangePercent,
+            currentLanguage,
+          ),
+          valueClassName: cn(
+            municipality.historicalEmissionChangePercent > 0
+              ? "text-pink-3"
+              : "text-orange-2",
+          ),
+        },
+        {
+          label: t("municipalityDetailPage.consumptionEmissionsPerCapita"),
+          value: localizeUnit(
+            municipality.totalConsumptionEmission,
+            currentLanguage,
+          ),
+          valueClassName: "text-orange-2",
+          unit: t("emissionsUnit"),
+        },
+      ]
+    : [];
 
   return stats;
 }
