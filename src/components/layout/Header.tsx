e0df54b422d6d605a5a7890879898336d92dec9c
Carl <carlwiren90@gmail.com>
2026-01-13 12:06:43 +0100

    Removed unused type

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,5 +1,5 @@
 import { BarChart3, Menu, X, Mail } from "lucide-react";
-import { To, useLocation } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 import { useState, useCallback, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { cn } from "@/lib/utils";
