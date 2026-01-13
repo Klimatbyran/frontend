63e529221c0301c0815257ba967549a5bda21053
JohnnyRico <156791432+CarlWiren90@users.noreply.github.com>
2026-01-13 11:25:06 +0100

    Update src/components/layout/Header.tsx
    Co-authored-by: Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -39,6 +39,7 @@ const NAV_LINKS: NavLink[] = [
   {
     label: "header.data",
     icon: <BarChart3 className="w-4 h-4" aria-hidden="true" />,
+    path: `/municipalities`
     sublinks: [
       {
         label: "header.companies",
