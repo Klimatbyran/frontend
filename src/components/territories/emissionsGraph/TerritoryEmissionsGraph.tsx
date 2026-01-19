b5dc48a56724e75ff503cc55aae1b6133bdb0c54
elvbom <ecdb@protonmail.com>
2026-01-19 20:46:37 +0100

    update import paths

--- src/components/territories/emissionsGraph/TerritoryEmissionsGraph.tsx
+++ src/components/territories/emissionsGraph/TerritoryEmissionsGraph.tsx
@@ -2,8 +2,8 @@ import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { DataPoint } from "@/types/emissions";
 import { SectorEmissions } from "@/types/emissions";
-import { OverviewChart } from "@/components/municipalities/emissionsGraph/OverviewChart";
-import { SectorsChart } from "@/components/municipalities/emissionsGraph/SectorsChart";
+import { OverviewChart } from "@/components/territories/emissionsGraph/OverviewChart";
+import { SectorsChart } from "@/components/territories/emissionsGraph/SectorsChart";
 
 type DataView = "overview" | "sectors";
 
