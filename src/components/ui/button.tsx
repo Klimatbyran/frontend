c7d6b3b3a680a6cc6b586f57868eb5126d454289
Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>
2026-02-17 15:02:44 +0100

    add delete company functionality

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
-const buttonVariants = cva(
+export const buttonVariants = cva(
   "inline-flex items-center justify-center rounded-full text-sm font-light transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-white disabled:pointer-events-none",
   {
     variants: {
