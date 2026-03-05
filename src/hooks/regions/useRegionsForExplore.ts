5f982128d14857c489cffc88152545e76a6f645c
Elvira <elvbom@users.noreply.github.com>
2026-03-05 10:26:29 +0100

    Update src/hooks/regions/useRegionsForExplore.ts
    Co-authored-by: Kayla Woodbury <59558092+kaylawoodbury@users.noreply.github.com>

--- src/hooks/regions/useRegionsForExplore.ts
+++ src/hooks/regions/useRegionsForExplore.ts
@@ -57,7 +57,7 @@ export function useRegionsForExplore() {
     data: regions = [],
     isLoading,
     error,
-  } = useQuery({
+} = useQuery<ApiRegion[], Error>({
     queryKey: ["regions"],
     queryFn: getRegions,
   });
